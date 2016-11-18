285e5f34322c2a12c9b9268dc81d9820d611f562
birgit <birgit.schachler@rl-institut.de>
2016-11-18 12:51:16 +0100

    Bugfixes in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(name='appBBB',
       author_email='elisa.gaudchau@rl-institut.de',
       description='A model of the heat and power systems of Brandenburg and Berlin',
       package_dir={'appBBB': 'appBBB'},
-      install_requires=['oemof = 0.0.9',
-                        'feedinlib = 0.0.10',
-                        'oemof.db = 0.0.3']
+      install_requires=['oemof == 0.0.9',
+                        'feedinlib == 0.0.10',
+                        'oemof.db'],
+      dependency_links = ['http://github.com/oemof/oemof.db/tarball/master'] 
       )
